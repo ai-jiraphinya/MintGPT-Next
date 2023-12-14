3f21674b77f7b715fb66cddd13586c0e8f40cec1
mint-jiraphinya <144240103+ai-jiraphinya@users.noreply.github.com>
2023-12-14 09:21:26 +0700

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+           MintBot Chat
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+           PhuPha-Daeng-LLaMA|vol.alpha
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
